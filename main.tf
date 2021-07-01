6024a27118e0c724e72c3976cff7d324775047c7
CISG-Olympus <CISGPlatform@gmail.com>
2021-07-01 14:20:05 +0100

    further refinements to main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,13 @@ terraform {
       version = "1.0.11"
     }
   }
-  
+}
+
 provider "intersight" {
-    apikey    = var.api_key
-    secretkey = var.secretkey
-    endpoint  = "https://intersight.com"
-  }
+  apikey    = var.api_key
+  secretkey = var.secretkey
+  endpoint  = "https://intersight.com"
+}
 
 #  backend "remote" {
 #    hostname     = "app.terraform.io"
